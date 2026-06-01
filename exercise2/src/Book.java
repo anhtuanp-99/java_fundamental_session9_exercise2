e53e3d0c81c6ecae7691ed10a611dc61669e3d45
anhtuanp-99 <anhtuanhl2004@gmail.com>
2026-06-01 22:38:17 +0700

    up ss9

--- exercise2/src/Book.java
+++ exercise2/src/Book.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    Scanner scanner = new Scanner(System.in);
+    // constructor
+    public Book(){
+        this.title = "Chưa đặt tên";
+        this.author = "Chưa có tác giả";
+        this.price = 0.0;
+    }
+
+    public Book(String title, String author, double price){
+        this.price = price;
+        this.title = title;
+        this.author = author;
+    }
+
+    // geters
+    public String getTitle(){
+        return title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    // setters
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor() {
+        this.author = author;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // printInfo
+    public void printInfo(){
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+
 }
--- exercise2/src/Main.java
+++ exercise2/src/Main.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner SCANNER = new Scanner(System.in);
+//        Book book1 = new Book("Thao túng tâm lý", "Anh Tuấn", 350000);
+//        book1.printInfo();
+
+        System.out.print("Nhập số lượng sách cần điền: ");
+        int amount = SCANNER.nextInt();
+        SCANNER.nextLine();
+
+        Book[] books = new Book[amount];
+
+        // nhập thông tin
+        for (int i = 0; i < amount; i++){
+            System.out.println("\nNhập sách thứ " + (i + 1));
+            System.out.print("Nhập tiêu đề: ");
+            String title = SCANNER.nextLine();
+            System.out.print("Nhập tác giả: ");
+            String author = SCANNER.nextLine();
+            System.out.print("Nhập giá: ");
+            double price = SCANNER.nextDouble();
+            SCANNER.nextLine();
+
+            books[i] = new Book(title, author, price);
+        }
+
+        System.out.println("\n----- LIST OF BOOK -----");
+        for (int i = 0; i < amount; i++){
+            books[i].printInfo();
+            System.out.println();
+        }
+
+        System.out.println(books[1].getPrice());
+        System.out.println(books[2].getAuthor());
+
+        SCANNER.close();
+    }
 }
